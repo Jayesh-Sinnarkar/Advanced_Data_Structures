94d3a127e79ca812f4a65bb3d3fd7865fa4916e6
jayesh <jayesh.sinnarkar@gmail.com>
2023-06-21 16:38:57 +0530

    Sync

---
+++ Assignments/LinkedLists/src/com/utils/SinglyLinkedList.java
@@ -0,0 +1,90 @@
+package com.utils;
+
+public class SinglyLinkedList {
+	// static class for Node
+	static class Node {
+		// Node class fields
+		private int data;
+		private Node next;
+
+		// Node class constructors
+		public Node() {
+			this.data = 0;
+			this.next = null;
+		}
+
+		public Node(int data) {
+			this.data = data;
+			this.next = null;
+		}
+
+	}
+
+	// SinglyLinkedList class fields
+	private Node head;
+
+	// SinglyLinkedList class methods
+	public SinglyLinkedList() {
+		head = null;
+	}
+
+	public void addFirst(int data) {
+		
+		Node newNode = new Node(data);
+		newNode.next = head;
+		head = newNode;
+	}
+
+	public void addLast(int data) {
+		Node newNode = new Node(data);
+		if (head == null) {
+			head = newNode;
+		} else {
+			Node trav = head;
+			while (trav.next != null)
+				trav = trav.next;
+			trav.next = newNode;
+		}
+	}
+	
+	public void addAtPos(int data, int pos)
+	{
+		if(head==null || pos<=1) //if head is null or pos is equals to or less than 1
+		{
+			addFirst(data);
+		}else {
+		Node newNode = new Node(data);
+		Node trav = head;
+		for (int i = 1; i < pos-1; i++) {
+			if(trav.next==null)//if pos is greater than length of list then add node to end of the list.
+				break;
+			trav = trav.next;
+		}
+		newNode.next = trav.next;
+		trav.next = newNode;
+		}
+		
+				
+	}
+	
+	public void delAtFirst() throws Exception {
+		if (head==null) {
+			throw new Exception("list is empty");
+		}
+		head=head.next;
+	}
+
+	public void display() {
+		Node trav = head;
+		System.out.println("List: ");
+		if (head == null)
+			System.out.println("List is empty");
+
+		while (trav != null) {
+
+			System.out.print(trav.data + "  ");
+			trav = trav.next;
+		}
+		System.out.println();
+	}
+}
---
+++ Assignments/LinkedLists/src/com/utils/SinglyLinkedListTester.java
@@ -0,0 +1,49 @@
+package com.utils;
+
+import java.util.Scanner;
+
+public class SinglyLinkedListTester {
+	
+	public static void main(String[] args)
+	{
+		int choice,val;
+		boolean exit=false;
+		SinglyLinkedList list = new SinglyLinkedList();
+		try(Scanner sc = new Scanner(System.in))
+		{
+			while(!exit) {
+				
+			System.out.println("1. Add First \n2. Add Last \n3.Display \n4.Exit");
+			choice = sc.nextInt();
+			
+			switch(choice)
+			{
+			case 1://Add first
+				System.out.println("Enter number to add: ");
+				val = sc.nextInt();
+				list.addFirst(val);
+				
+				break;
+			case 2://Add Last
+				System.out.println("Enter number to add: ");
+				val = sc.nextInt();
+				list.addLast(val);
+				break;
+			case 3://Display
+				list.display();
+				break;
+			case 4://Exit
+				exit=true;
+				break;
+			}
+			
+			}
+			
+		}catch(Exception e)
+		{
+			System.out.println(e.getMessage());
+		}
+		
+	}
+
+}
